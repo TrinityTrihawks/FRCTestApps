a6d7ea5784483de538b93853ac220e9a542ae49e
DouglasErickson <douglas.erickson@gmail.com>
2018-01-13 12:06:03 -0600

    UltrasonicTestApp

---
+++ UltrasonicConsole/src/com/frc/team4215/UltrasonicConsole.java
@@ -0,0 +1,24 @@
+package com.frc.team4215;
+
+public class UltrasonicConsole {
+
+	public static void main(String[] args) {
+
+//	    UltrasonicReader reader = UltrasonicReader.Create("/dev/ttyUSB0");
+	    UltrasonicReader reader = UltrasonicReader.Create("COM3");
+	    
+	    if (reader == null) {
+			System.out.println("No reader found!");
+			return;
+	    }
+	    
+	    while (true) {
+	    	try {
+    			System.out.println("dist: " + reader.getDistance());
+    			Thread.sleep(250);
+	    	} catch (InterruptedException e) {
+	    		System.out.println("Exception: " + e.getMessage());
+	    	}
+	    }
+	}
+}
